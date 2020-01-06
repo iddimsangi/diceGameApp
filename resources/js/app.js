aaa17569e95a241f596cce9e779c3fc9477414e6
iddimsangi <adammsangi@gmail.com>
2020-01-06 15:49:17 +0300

    make a none display of dice

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,11 @@ activePlayer = 0;
 document.querySelector('#score-'+activePlayer).textContent = '0';
 document.querySelector('#score-1').textContent = '0';
 document.querySelector('#current-'+activePlayer).textContent = '0';
+document.querySelector('.dice').style.display = 'none';
 
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+})
 
 
 
